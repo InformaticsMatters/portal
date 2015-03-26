789c2be6c9155a0b5f4fd732d1c846cfe3e29447
mariapaz <jonimigue2215>
2015-03-26 18:32:19 -0300

    Styles and overrides.

--- toolkit/src/toolkit/wicket/semantic/SemanticResourceReference.java
+++ toolkit/src/toolkit/wicket/semantic/SemanticResourceReference.java
@@ -1,5 +1,7 @@
 package toolkit.wicket.semantic;
 
+import com.inmethod.grid.common.AbstractGrid;
+import com.vaynberg.wicket.select2.ApplicationSettings;
 import org.apache.wicket.markup.head.*;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
@@ -26,7 +28,12 @@ public class SemanticResourceReference extends JavaScriptResourceReference {
     public Iterable<? extends HeaderItem> getDependencies() {
         JavaScriptReferenceHeaderItem jquery = JavaScriptHeaderItem.forReference(JQueryResourceReference.get());
         CssReferenceHeaderItem style = CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/semantic.min.css"));
-        return Arrays.asList(jquery, style);
+        CssReferenceHeaderItem overrides = CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/semantic-overrides.css"));
+        CssReferenceHeaderItem easyGridOverrides = CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/easygrid-overrides.css"));
+        CssReferenceHeaderItem inmethodStyle = CssHeaderItem.forReference(new CssResourceReference(AbstractGrid.class, "res/style.css"));
+        CssReferenceHeaderItem select2Style = CssHeaderItem.forReference(new CssResourceReference(ApplicationSettings.class, "res/select2.css"));
+        CssReferenceHeaderItem select2Overrides = CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/select2-overrides.css"));
+        return Arrays.asList(jquery, style, overrides, inmethodStyle, easyGridOverrides, select2Style, select2Overrides);
     }
 
 }
