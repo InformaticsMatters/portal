d0b1b3f6ef55a9012f993da81e3e48e5f1677a39
mariapaz <jonimigue2215>
2015-04-13 18:40:07 -0300

    Left sidebar first pass.

--- portal-app/src/portal/webapp/TreeGridVisualizerPage.java
+++ portal-app/src/portal/webapp/TreeGridVisualizerPage.java
@@ -1,12 +1,20 @@
 package portal.webapp;
 
+import com.vaynberg.wicket.select2.Select2Choice;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.navigation.paging.AjaxPagingNavigation;
+import org.apache.wicket.cdi.CdiContainer;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import portal.integration.PropertyDefinition;
 import portal.service.api.DatasetDescriptor;
 import portal.service.api.DatasetService;
 import toolkit.wicket.semantic.NotifierProvider;
@@ -15,11 +23,16 @@ import toolkit.wicket.semantic.SemanticResourceReference;
 import javax.inject.Inject;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
 public class TreeGridVisualizerPage extends WebPage {
 
     private AjaxLink leftSidebar;
     private AjaxLink rightSidebar;
+
+    private Form<ColumnsData> form;
 
     @Inject
     private NotifierProvider notifierProvider;
@@ -27,6 +40,7 @@ public class TreeGridVisualizerPage extends WebPage {
     private DatasetService service;
     private AjaxPagingNavigation navigation;
     private TreeGridVisualizer treeGridVisualizer;
+    private ListView<String> columnListView;
 
     public TreeGridVisualizerPage(DatasetDescriptor datasetDescriptor) {
         notifierProvider.createNotifier(this, "notifier");
@@ -44,6 +58,8 @@ public class TreeGridVisualizerPage extends WebPage {
             }
         });
         addActions();
+        addForm();
+        addColumnsListView();
     }
 
     @Override
@@ -78,5 +94,45 @@ public class TreeGridVisualizerPage extends WebPage {
             }
         };
         add(rightSidebar);
+    }
+
+    private void addForm() {
+        form = new Form<>("form");
+        form.setModel(new CompoundPropertyModel<>(new ColumnsData()));
+        form.setOutputMarkupId(true);
+        add(form);
+
+        PropertyDefinitionProvider propertyDefinitionProvider = new PropertyDefinitionProvider();
+        CdiContainer.get().getNonContextualManager().postConstruct(propertyDefinitionProvider);
+
+        Select2Choice<PropertyDefinition> propertyDefinition = new Select2Choice<>("propertyDefinition");
+        propertyDefinition.setProvider(propertyDefinitionProvider);
+        propertyDefinition.getSettings().setMinimumInputLength(4);
+        propertyDefinition.setOutputMarkupId(true);
+        form.add(propertyDefinition);
+    }
+
+    private void addColumnsListView() {
+        List columnList = Arrays.asList(new String[]{"a", "b", "c"});
+        columnListView = new ListView("columnList", columnList) {
+            protected void populateItem(ListItem item) {
+                item.add(new Label("columnName", item.getModel()));
+            }
+        };
+        add(columnListView);
+    }
+
+    private class ColumnsData implements Serializable {
+
+        private PropertyDefinition propertyDefinition;
+
+
+        public PropertyDefinition getPropertyDefinition() {
+            return propertyDefinition;
+        }
+
+        public void setPropertyDefinition(PropertyDefinition propertyDefinition) {
+            this.propertyDefinition = propertyDefinition;
+        }
     }
 }
