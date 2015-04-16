b04e384e115d48ed588c0de49e09c33868d70953
mariapaz <jonimigue2215>
2015-04-16 18:27:21 -0300

    Right sidebar.

--- portal-app/src/portal/webapp/DrugWorkbenchPage.java
+++ portal-app/src/portal/webapp/DrugWorkbenchPage.java
@@ -1,8 +1,11 @@
 package portal.webapp;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import portal.service.api.DatasetService;
 import toolkit.wicket.semantic.NotifierProvider;
 import toolkit.wicket.semantic.SemanticResourceReference;
@@ -10,6 +13,8 @@ import toolkit.wicket.semantic.SemanticResourceReference;
 import javax.inject.Inject;
 
 public class DrugWorkbenchPage extends WebPage {
+
+    private AjaxLink rightSidebar;
 
     @Inject
     private NotifierProvider notifierProvider;
@@ -19,13 +24,26 @@ public class DrugWorkbenchPage extends WebPage {
     public DrugWorkbenchPage() {
         notifierProvider.createNotifier(this, "notifier");
         add(new MenuPanel("menuPanel"));
+        addActions();
     }
 
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
+        response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(PortalWebApplication.class, "resources/lac.js")));
         response.render(JavaScriptHeaderItem.forReference(SemanticResourceReference.get()));
     }
 
+    private void addActions() {
+
+        rightSidebar = new AjaxLink("rightSidebar") {
+
+            @Override
+            public void onClick(AjaxRequestTarget ajaxRequestTarget) {
+                ajaxRequestTarget.appendJavaScript("rightSideBarToggle()");
+            }
+        };
+        add(rightSidebar);
+    }
 
 }
