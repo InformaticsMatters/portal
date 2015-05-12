3c6fce1855f2175545caf9f85053275cddef530d
gustavosantucho <gustavosantucho@gmail.com>
2015-05-12 11:40:35 -0300

    Fixed dataset loading for workflow page

--- portal-app/src/portal/webapp/DatasetsPanel.java
+++ portal-app/src/portal/webapp/DatasetsPanel.java
@@ -6,28 +6,28 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import portal.integration.DatamartSession;
 import portal.service.api.DatasetDescriptor;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.inject.Inject;
 
 /**
  * @author simetrias
  */
 public class DatasetsPanel extends Panel {
 
-    private List<DatasetDescriptor> datasetDescriptorList;
     private ListView<DatasetDescriptor> listView;
+    @Inject
+    private DatamartSession datamartSession;
 
     public DatasetsPanel(String id) {
         super(id);
         addDatasets();
-        datasetDescriptorList = new ArrayList<>();
     }
 
     private void addDatasets() {
-
-        listView = new ListView<DatasetDescriptor>("descriptors", new ArrayList<>()) {
+        datamartSession.loadDatamartDatasetList();
+        listView = new ListView<DatasetDescriptor>("descriptors", datamartSession.getDatasetDescriptorList()) {
 
             @Override
             protected void populateItem(ListItem<DatasetDescriptor> listItem) {
@@ -35,6 +35,7 @@ public class DatasetsPanel extends Panel {
                 listItem.add(new Label("description", datasetDescriptor.getDescription()));
                 listItem.add(new Label("rowCount", datasetDescriptor.getRowCount()));
                 listItem.add(new IndicatingAjaxLink("open") {
+
 
                     @Override
                     public void onClick(AjaxRequestTarget ajaxRequestTarget) {
@@ -45,38 +46,5 @@ public class DatasetsPanel extends Panel {
             }
         };
         add(listView);
-
-            /*  List<DatasetData> datasetDataList = new ArrayList<DatasetData>();
-        datasetDataList.add(new DatasetData());
-        datasetDataList.add(new DatasetData());
-        datasetDataList.add(new DatasetData());
-
-        ListView<DatasetData> datasets = new ListView<DatasetData>("datasets", datasetDataList) {
-
-            @Override
-            protected void populateItem(ListItem<DatasetData> components) {
-                components.add(new WebMarkupContainer("dataset"));
-            }
-        };
-        add(datasets);   */
     }
-
-
-    public void setDatasetDescriptorList(List<DatasetDescriptor> datasetDescriptorList) {
-        this.datasetDescriptorList = datasetDescriptorList;
-        listView.setList(datasetDescriptorList);
-    }
-
-   /* class DatasetData implements Serializable {
-
-        private String name;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }  */
 }
