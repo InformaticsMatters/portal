1c73e1fd2383e318de68461f7ac30b8039013a5f
mariapaz <jonimigue2215>
2015-05-12 11:02:59 -0300

    Datasets.

--- portal-app/src/portal/webapp/DatasetsPanel.java
+++ portal-app/src/portal/webapp/DatasetsPanel.java
@@ -1,21 +1,52 @@
 package portal.webapp;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import portal.service.api.DatasetDescriptor;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author simetrias
  */
 public class DatasetsPanel extends Panel {
 
+    private List<DatasetDescriptor> datasetDescriptorList;
+    private ListView<DatasetDescriptor> listView;
+
     public DatasetsPanel(String id) {
         super(id);
         addDatasets();
+        datasetDescriptorList = new ArrayList<>();
     }
 
     private void addDatasets() {
-      /*  List<DatasetData> datasetDataList = new ArrayList<DatasetData>();
+
+        listView = new ListView<DatasetDescriptor>("descriptors", new ArrayList<>()) {
+
+            @Override
+            protected void populateItem(ListItem<DatasetDescriptor> listItem) {
+                DatasetDescriptor datasetDescriptor = listItem.getModelObject();
+                listItem.add(new Label("description", datasetDescriptor.getDescription()));
+                listItem.add(new Label("rowCount", datasetDescriptor.getRowCount()));
+                listItem.add(new IndicatingAjaxLink("open") {
+
+                    @Override
+                    public void onClick(AjaxRequestTarget ajaxRequestTarget) {
+                        TreeGridVisualizerPage page = new TreeGridVisualizerPage(datasetDescriptor);
+                        setResponsePage(page);
+                    }
+                });
+            }
+        };
+        add(listView);
+
+            /*  List<DatasetData> datasetDataList = new ArrayList<DatasetData>();
         datasetDataList.add(new DatasetData());
         datasetDataList.add(new DatasetData());
         datasetDataList.add(new DatasetData());
@@ -30,7 +61,13 @@ public class DatasetsPanel extends Panel {
         add(datasets);   */
     }
 
-    class DatasetData implements Serializable {
+
+    public void setDatasetDescriptorList(List<DatasetDescriptor> datasetDescriptorList) {
+        this.datasetDescriptorList = datasetDescriptorList;
+        listView.setList(datasetDescriptorList);
+    }
+
+   /* class DatasetData implements Serializable {
 
         private String name;
 
@@ -41,5 +78,5 @@ public class DatasetsPanel extends Panel {
         public void setName(String name) {
             this.name = name;
         }
-    }
+    }  */
 }
