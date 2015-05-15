fd6aa6ed24192b9c2798e644720b098da34bca3b
mariapaz <jonimigue2215>
2015-05-15 17:32:57 -0300

    Footer.

--- portal-app/src/portal/webapp/PortalHomePage.java
+++ portal-app/src/portal/webapp/PortalHomePage.java
@@ -2,9 +2,11 @@ package portal.webapp;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import portal.integration.DatamartSession;
 import portal.service.api.DatasetDescriptor;
@@ -48,6 +50,7 @@ public class PortalHomePage extends WebPage {
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
         response.render(JavaScriptHeaderItem.forReference(SemanticResourceReference.get()));
+        response.render(CssHeaderItem.forReference(new CssResourceReference(WorkflowPage.class, "resources/lac.css")));
         response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(PortalWebApplication.class, "resources/lac.js")));
     }
 
