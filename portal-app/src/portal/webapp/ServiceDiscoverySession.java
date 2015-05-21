14ea8934131803803ce1541e407ca49c38d856a3
gustavosantucho <gustavosantucho@gmail.com>
2015-05-21 16:14:01 -0300

    Service Discovery Session

--- portal-app/src/portal/webapp/ServiceDiscoverySession.java
+++ portal-app/src/portal/webapp/ServiceDiscoverySession.java
@@ -3,6 +3,9 @@ package portal.webapp;
 import portal.service.api.ServiceDescriptor;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,7 +16,15 @@ public class ServiceDiscoverySession implements Serializable {
     private Map<Long, ServiceDescriptor> serviceDescriptorMap;
 
     public void loadServiceList() {
+        serviceDescriptorMap = new HashMap<Long, ServiceDescriptor>();
+        ServiceDescriptor serviceDescriptor;
+        serviceDescriptor = new ServiceDescriptor();
+        serviceDescriptor.setName("Service 1");
+        serviceDescriptorMap.put(1l, serviceDescriptor);
+    }
 
+    public List<ServiceDescriptor> getServiceDescriptorList() {
+        return new ArrayList<>(serviceDescriptorMap.values());
     }
 
 }
