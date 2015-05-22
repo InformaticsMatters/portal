be899407663baa1f6da8d9ce5e0a61a4212e9672
mariapaz <jonimigue2215>
2015-05-22 15:52:45 -0300

    Canvas service popup.

--- portal-app/src/portal/webapp/ServiceCanvasItemPanel.java
+++ portal-app/src/portal/webapp/ServiceCanvasItemPanel.java
@@ -1,5 +1,7 @@
 package portal.webapp;
 
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 
@@ -8,11 +10,32 @@ import org.apache.wicket.markup.html.panel.Panel;
  */
 public class ServiceCanvasItemPanel extends Panel {
 
+    private ServiceCanvasItemPopupPanel serviceCanvasItemPopup;
+
     public ServiceCanvasItemPanel(String id, ServiceCanvasItemData model) {
         super(id);
         setOutputMarkupId(true);
+        addServiceCanvasItemPopup();
 
         add(new Label("id", model.getServiceDescriptor().getId()));
         add(new Label("name", model.getServiceDescriptor().getName()));
+
+        add(new AjaxEventBehavior("click") {
+            @Override
+            protected void onEvent(AjaxRequestTarget ajaxRequestTarget) {
+                // serviceCanvasItemPopup.setDefaultModelObject(serviceDescriptor);
+                serviceCanvasItemPopup.setVisible(true);
+                ajaxRequestTarget.add(serviceCanvasItemPopup);
+                String js = "$('.canvas-item-content').popup({popup: '.ui.serviceCanvasItemPopup.popup', on : 'click'}).popup('toggle')";
+                ajaxRequestTarget.appendJavaScript(js);
+            }
+        });
+    }
+
+    private void addServiceCanvasItemPopup() {
+        serviceCanvasItemPopup = new ServiceCanvasItemPopupPanel("serviceCanvasItemPopup");
+        // serviceCanvasItemPopup.setDefaultModel(new CompoundPropertyModel<>(null));
+        serviceCanvasItemPopup.setVisible(false);
+        add(serviceCanvasItemPopup);
     }
 }
---
+++ portal-app/src/portal/webapp/ServiceCanvasItemPopupPanel.java
@@ -0,0 +1,15 @@
+package portal.webapp;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * Created by mariapaz on 5/22/15.
+ */
+public class ServiceCanvasItemPopupPanel extends Panel {
+
+    public ServiceCanvasItemPopupPanel(String id) {
+        super(id);
+        setOutputMarkupId(true);
+        setOutputMarkupPlaceholderTag(true);
+    }
+}
