8f799739083c796d36ce3549e5fbfc6fb0356be2
mariapaz <jonimigue2215>
2015-06-01 13:02:29 -0300

    Sketcher in service popup.

--- portal-app/src/portal/webapp/ServiceCanvasItemPopupPanel.java
+++ portal-app/src/portal/webapp/ServiceCanvasItemPopupPanel.java
@@ -3,6 +3,7 @@ package portal.webapp;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.panel.Panel;
+import toolkit.wicket.marvin4js.MarvinSketcher;
 
 /**
  * @author simetrias
@@ -10,11 +11,13 @@ import org.apache.wicket.markup.html.panel.Panel;
 public class ServiceCanvasItemPopupPanel extends Panel {
 
     private ServiceCanvasItemPanel.Callbacks callbacks;
+    private MarvinSketcher marvinSketcherPanel;
 
     public ServiceCanvasItemPopupPanel(String id, ServiceCanvasItemPanel.Callbacks callbacks) {
         super(id);
         setOutputMarkupId(true);
         setOutputMarkupPlaceholderTag(true);
+
         add(new AjaxLink("delete") {
 
             @Override
@@ -22,6 +25,29 @@ public class ServiceCanvasItemPopupPanel extends Panel {
                 callbacks.onDelete();
             }
         });
+
+        add(new AjaxLink("sketcher") {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                marvinSketcherPanel.showModal();
+            }
+        });
+
+        marvinSketcherPanel = new MarvinSketcher("marvinSketcherPanel", "modalElement");
+        marvinSketcherPanel.setCallbacks(new MarvinSketcher.Callbacks() {
+
+            @Override
+            public void onSubmit() {
+                marvinSketcherPanel.hideModal();
+            }
+
+            @Override
+            public void onCancel() {
+            }
+        });
+        add(marvinSketcherPanel);
+
     }
 
 
