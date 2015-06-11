de3a7a52f68387ead23c941037414ac19292c7f0
mariapaz <jonimigue2215>
2015-06-11 19:53:19 -0300

    Jobs panel.

---
+++ portal-app/src/portal/webapp/JobsPanel.java
@@ -0,0 +1,14 @@
+package portal.webapp;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * @author simetrias
+ */
+public class JobsPanel extends Panel {
+
+    public JobsPanel(String id) {
+        super(id);
+    }
+
+}
