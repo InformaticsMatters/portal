7aa16d27a58099811b859b0d5588f8a273f012db
mariapaz <jonimigue2215>
2015-07-27 20:51:05 -0300

    File drop.

--- portal-app/src/portal/webapp/resources/lac.js
+++ portal-app/src/portal/webapp/resources/lac.js
@@ -124,18 +124,20 @@ if (window.FileReader) {
         });
 
     });
-}
 
-
-function addEventHandler(obj, evt, handler) {
-    if (obj.addEventListener) {
-        // W3C method
-        obj.addEventListener(evt, handler, false);
-    } else if (obj.attachEvent) {
-        // IE method.
-        obj.attachEvent('on' + evt, handler);
-    } else {
-        // Old school method.
-        obj['on' + evt] = handler;
+    function addEventHandler(obj, evt, handler) {
+        if (obj.addEventListener) {
+            // W3C method
+            obj.addEventListener(evt, handler, false);
+        } else if (obj.attachEvent) {
+            // IE method.
+            obj.attachEvent('on' + evt, handler);
+        } else {
+            // Old school method.
+            obj['on' + evt] = handler;
+        }
     }
 }
+
+
+
