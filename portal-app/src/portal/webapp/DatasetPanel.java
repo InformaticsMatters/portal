724a7c8cda4b2565b22145c6355d11bd22d0bba8
gustavosantucho <gustavosantucho@gmail.com>
2015-07-28 19:10:42 -0300

    Close popup on Delete

--- portal-app/src/portal/webapp/DatasetPanel.java
+++ portal-app/src/portal/webapp/DatasetPanel.java
@@ -20,6 +20,7 @@ public class DatasetPanel extends Panel {
 
     @Inject
     private PopupContainerProvider popupContainerProvider;
+    private IndicatingAjaxLink openPopupLink;
 
     public DatasetPanel(String id, IDatasetDescriptor datasetDescriptor, Callbacks callbacks) {
         super(id);
@@ -27,36 +28,31 @@ public class DatasetPanel extends Panel {
         this.datasetDescriptor = datasetDescriptor;
         setOutputMarkupId(true);
         addComponents();
-        addDatasetPopupPanel();
+        createDatasetPopupPanel();
     }
 
     private void addComponents() {
         add(new Label("description", datasetDescriptor.getDescription()));
         add(new Label("rowCount", datasetDescriptor.getRowCount()));
 
-        add(new IndicatingAjaxLink("openPopup") {
+        openPopupLink = new IndicatingAjaxLink("openPopup") {
 
             @Override
             public void onClick(AjaxRequestTarget ajaxRequestTarget) {
                 popupContainerProvider.setPopupContentForPage(getPage(), datasetPopupPanel);
                 popupContainerProvider.refreshContainer(getPage(), ajaxRequestTarget);
-                String js = "$('#" + getMarkupId() + "').popup({popup: $('#" + datasetPopupPanel.getMarkupId() + "').find('.ui.datasetPopup.popup'), on : 'click'}).popup('toggle')";
+                String js = "$('#" + getMarkupId() + "').popup({target: $('#" + openPopupLink.getMarkupId() + "'), popup: $('#" + datasetPopupPanel.getMarkupId() + "').find('.ui.datasetPopup.popup'), on : 'click'}).popup('toggle')";
                 ajaxRequestTarget.appendJavaScript(js);
-                /*
-                datasetPopupPanel.setVisible(true);
-                ajaxRequestTarget.add(datasetPopupPanel);
-                String js = "$('#" + getMarkupId() + "').popup({popup: $('#" + DatasetPanel.this.getMarkupId() + "').find('.ui.datasetPopup.popup'), on : 'click'}).popup('toggle')";
-                ajaxRequestTarget.appendJavaScript(js);
-                */
             }
-        });
+        };
+        add(openPopupLink);
     }
 
-    private void addDatasetPopupPanel() {
-        datasetPopupPanel = new DatasetPopupPanel("content", datasetDescriptor, callbacks::onDelete);
-        // datasetPopupPanel = new DatasetPopupPanel("popupPanel", datasetDescriptor, callbacks::onDelete);
-        // datasetPopupPanel.setVisible(false);
-        // add(datasetPopupPanel);
+    private void createDatasetPopupPanel() {
+        datasetPopupPanel = new DatasetPopupPanel("content", datasetDescriptor, () -> {
+            popupContainerProvider.refreshContainer(getPage(), getRequestCycle().find(AjaxRequestTarget.class));
+            callbacks.onDelete();
+        });
     }
 
     public interface Callbacks extends Serializable {
