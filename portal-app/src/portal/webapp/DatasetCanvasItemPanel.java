f46295a853d108d85e8f5811e0866c3b0eada00d
mariapaz <jonimigue2215>
2015-07-30 12:15:02 -0300

    Canvas popup.

--- portal-app/src/portal/webapp/DatasetCanvasItemPanel.java
+++ portal-app/src/portal/webapp/DatasetCanvasItemPanel.java
@@ -1,9 +1,12 @@
 package portal.webapp;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxLink;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+
+import javax.inject.Inject;
+import java.io.Serializable;
 
 /**
  * @author simetrias
@@ -11,27 +14,52 @@ import org.apache.wicket.markup.html.panel.Panel;
 public class DatasetCanvasItemPanel extends Panel {
 
     private final DatasetCanvasItemData data;
+    private final Callbacks callbacks;
+    private DatasetCanvasItemPopupPanel datasetCanvasItemPopupPanel;
 
-    public DatasetCanvasItemPanel(String id, DatasetCanvasItemData data) {
+    @Inject
+    private PopupContainerProvider popupContainerProvider;
+    private AjaxLink openPopupLink;
+
+    public DatasetCanvasItemPanel(String id, DatasetCanvasItemData data, Callbacks callbacks) {
         super(id);
         this.data = data;
+        this.callbacks = callbacks;
         setOutputMarkupId(true);
+
+        createDatasetCanvasItemPopupPanel();
 
         add(new Label("id", data.getDatasetDescriptor().getId()));
         add(new Label("description", data.getDatasetDescriptor().getDescription()));
         add(new Label("rowCount", data.getDatasetDescriptor().getRowCount()));
-        add(new IndicatingAjaxLink("open") {
+
+        openPopupLink = new AjaxLink("openPopup") {
 
             @Override
             public void onClick(AjaxRequestTarget ajaxRequestTarget) {
-                TreeGridVisualizerPage page = new TreeGridVisualizerPage(data.getDatasetDescriptor());
-                setResponsePage(page);
+                popupContainerProvider.setPopupContentForPage(getPage(), datasetCanvasItemPopupPanel);
+                popupContainerProvider.refreshContainer(getPage(), ajaxRequestTarget);
+                String js = "$('#" + getMarkupId() + "').popup({simetriasPatch: true, popup: $('#" + datasetCanvasItemPopupPanel.getMarkupId() + "').find('.ui.datasetPopup.popup'), on : 'click'}).popup('toggle')";
+                ajaxRequestTarget.appendJavaScript(js);
             }
-        });
+        };
+        add(openPopupLink);
+    }
 
+    private void createDatasetCanvasItemPopupPanel() {
+        datasetCanvasItemPopupPanel = new DatasetCanvasItemPopupPanel("content", datasetDescriptor, () -> {
+            popupContainerProvider.refreshContainer(getPage(), getRequestCycle().find(AjaxRequestTarget.class));
+            callbacks.onDelete();
+        });
     }
 
     public DatasetCanvasItemData getData() {
         return data;
+    }
+
+    public interface Callbacks extends Serializable {
+
+        void onDelete();
+
     }
 }
---
+++ portal-app/src/portal/webapp/DatasetCanvasItemPopupPanel.java
@@ -0,0 +1,13 @@
+package portal.webapp;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * @author simetrias
+ */
+public class DatasetCanvasItemPopupPanel extends Panel {
+
+    public DatasetCanvasItemPopupPanel(String id) {
+        super(id);
+    }
+}
