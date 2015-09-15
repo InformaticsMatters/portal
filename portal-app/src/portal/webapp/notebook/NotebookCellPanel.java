49b9a4521c1dde01ed0ba705b452360cdec14422
mariapaz <jonimigue2215>
2015-09-15 16:12:45 -0300

    Notebook cell panel.

---
+++ portal-app/src/portal/webapp/notebook/NotebookCellPanel.java
@@ -0,0 +1,15 @@
+package portal.webapp.notebook;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * @author simetrias
+ */
+
+public class NotebookCellPanel extends Panel {
+
+    public NotebookCellPanel(String id) {
+        super(id);
+
+    }
+}
