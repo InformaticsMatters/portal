61180c3ef5f7b1e9698e40666fe6e9a51d0a06aa
gustavosantucho <gustavosantucho@gmail.com>
2015-09-16 11:07:41 -0300

    Cleanup

--- portal-app/src/portal/chemcentral/ListDatasetDescriptorFilter.java
+++
@@ -1,7 +0,0 @@
-package portal.chemcentral;
-
-/**
- * @author simetrias
- */
-public class ListDatasetDescriptorFilter {
-}
--- portal-app/src/portal/webapp/visualizers/TreeGridVisualizerPage.java
+++ portal-app/src/portal/webapp/visualizers/TreeGridVisualizerPage.java
@@ -1,65 +1,37 @@
 package portal.webapp.visualizers;
 
-import com.vaynberg.wicket.select2.Select2Choice;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.cdi.CdiContainer;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
-import portal.chemcentral.ChemcentralSession;
-import portal.chemcentral.PropertyData;
-import portal.chemcentral.PropertyDefinition;
 import portal.dataset.IDatasetDescriptor;
 import portal.webapp.FooterPanel;
 import portal.webapp.MenuPanel;
 import portal.webapp.PortalWebApplication;
-import portal.webapp.PropertyDefinitionProvider;
 import portal.webapp.workflow.DatasetsSession;
 import toolkit.wicket.semantic.NotifierProvider;
 import toolkit.wicket.semantic.SemanticResourceReference;
 
 import javax.inject.Inject;
-import javax.swing.tree.TreeModel;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 public class TreeGridVisualizerPage extends WebPage {
 
     private final IDatasetDescriptor datasetDescriptor;
-    private Form<PropertyDefinitionData> form;
-
     private TreeGridVisualizer treeGridVisualizer;
-    private ListView<String> columnsListView;
-    private WebMarkupContainer columnsContainer;
     private MenuPanel menuPanel;
 
     @Inject
     private NotifierProvider notifierProvider;
-    @Inject
-    private ChemcentralSession chemcentralSession;
     @Inject
     private DatasetsSession datasetsSession;
 
     public TreeGridVisualizerPage(IDatasetDescriptor datasetDescriptor) {
         this.datasetDescriptor = datasetDescriptor;
         notifierProvider.createNotifier(this, "notifier");
-        addPanels();
+        addFooter();
         addOrReplaceTreeGridVisualizer(datasetDescriptor);
-        addColumnsForm();
-        addColumnsListView();
     }
 
     @Override
@@ -70,7 +42,7 @@ public class TreeGridVisualizerPage extends WebPage {
         response.render(CssHeaderItem.forReference(new CssResourceReference(PortalWebApplication.class, "resources/lac.css")));
     }
 
-    private void addPanels() {
+    private void addFooter() {
         add(new FooterPanel("footerPanel"));
     }
 
@@ -80,100 +52,4 @@ public class TreeGridVisualizerPage extends WebPage {
         TreeGridNavigationPanel treeGridNavigation = new TreeGridNavigationPanel("treeGridNavigation", treeGridVisualizer);
         addOrReplace(treeGridNavigation);
     }
-
-    private void addColumnsForm() {
-        form = new Form<>("form");
-        form.setModel(new CompoundPropertyModel<>(new PropertyDefinitionData()));
-        form.setOutputMarkupId(true);
-        add(form);
-
-        // why not just to inject the provider dependant scope?
-        PropertyDefinitionProvider propertyDefinitionProvider = new PropertyDefinitionProvider();
-        CdiContainer.get().getNonContextualManager().postConstruct(propertyDefinitionProvider);
-
-        Select2Choice<PropertyDefinition> definitionChoice = new Select2Choice<>("propertyDefinition");
-        definitionChoice.setProvider(propertyDefinitionProvider);
-        definitionChoice.getSettings().setMinimumInputLength(4);
-        definitionChoice.setOutputMarkupId(true);
-        form.add(definitionChoice);
-
-        definitionChoice.add(new AjaxFormComponentUpdatingBehavior("onchange") {
-
-            @Override
-            protected void onUpdate(AjaxRequestTarget ajaxRequestTarget) {
-                onPropertyDefinitionSelected();
-            }
-        });
-    }
-
-    private void addColumnsListView() {
-        columnsContainer = new WebMarkupContainer("columnsContainer");
-        columnsContainer.setOutputMarkupId(true);
-        columnsListView = new ListView<String>("columnList") {
-
-            @Override
-            protected void populateItem(ListItem<String> item) {
-                item.add(new Label("label", item.getModel()));
-                item.add(new AjaxLink("add") {
-
-                    @Override
-                    public void onClick(AjaxRequestTarget ajaxRequestTarget) {
-                        chemcentralSession.addPropertyToDataset(datasetDescriptor, item.getModelObject());
-                        addOrReplaceTreeGridVisualizer(datasetDescriptor);
-                        ajaxRequestTarget.add(treeGridVisualizer);
-
-                        TreeModel treeModel = treeGridVisualizer.getTree().getModelObject();
-
-
-                    }
-                });
-            }
-        };
-        columnsContainer.add(columnsListView);
-        add(columnsContainer);
-    }
-
-    private void onPropertyDefinitionSelected() {
-        ArrayList<String> columnList = new ArrayList<>();
-        PropertyDefinition propertyDefinition = form.getModelObject().getPropertyDefinition();
-        if (propertyDefinition != null) {
-            List<PropertyData> propertyDataList = chemcentralSession.listPropertyData(datasetDescriptor, propertyDefinition);
-            if (propertyDataList != null && propertyDataList.size() > 0) {
-                PropertyData propertyData = propertyDataList.get(0);
-                Set<String> keyset = propertyData.getData().keySet();
-                columnList.addAll(keyset);
-            } else {
-                System.out.println("No data for that property");
-            }
-        }
-        columnsListView.setList(columnList);
-        getRequestCycle().find(AjaxRequestTarget.class).add(columnsContainer);
-    }
-
-    private class PropertyDefinitionData implements Serializable {
-
-        private PropertyDefinition propertyDefinition;
-
-        public PropertyDefinition getPropertyDefinition() {
-            return propertyDefinition;
-        }
-
-        public void setPropertyDefinition(PropertyDefinition propertyDefinition) {
-            this.propertyDefinition = propertyDefinition;
-        }
-    }
-
-    private class ColumnData implements Serializable {
-
-        private String description;
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-    }
-
 }
