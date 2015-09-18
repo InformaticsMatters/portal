9b3a415f2101de7267713e1e696275d77208b658
mariapaz <jonimigue2215>
2015-09-18 18:52:21 -0300

    Canvas items.

--- portal-app/src/portal/webapp/notebook/TestCanvasCellPage.java
+++ portal-app/src/portal/webapp/notebook/TestCanvasCellPage.java
@@ -1,31 +1,83 @@
 package portal.webapp.notebook;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.resource.JQueryResourceReference;
+import portal.webapp.PortalHomePage;
+import toolkit.wicket.semantic.SemanticResourceReference;
 
 /**
  * Created by mariapaz on 9/18/15.
  */
 public class TestCanvasCellPage extends WebPage {
 
+    private WebMarkupContainer plumbContainer;
+
+    private AjaxLink fileUploadButton;
+
     private FileUploadCanvasItemPanel fileUploadCanvasItemPanel;
     private GroovyScriptCanvasItemPanel groovyScriptCanvasItemPanel;
     private TableViewCanvasItemPanel tableViewCanvasItemPanel;
 
     public TestCanvasCellPage() {
-        addPanels();
+        addCanvas();
+        addLinks();
     }
 
-    private void addPanels() {
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        response.render(JavaScriptHeaderItem.forReference(SemanticResourceReference.get()));
+        response.render(JavaScriptHeaderItem.forReference(JQueryResourceReference.get()));
+        response.render(CssHeaderItem.forReference(new CssResourceReference(PortalHomePage.class, "resources/lac.css")));
+        response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(PortalHomePage.class, "resources/dom.jsPlumb-1.7.5.js")));
+        response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(PortalHomePage.class, "resources/Canvas.js")));
+        response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(PortalHomePage.class, "resources/lac.js")));
+        response.render(OnDomReadyHeaderItem.forScript("init();"));
+    }
+
+    private void addCanvas() {
+        plumbContainer = new WebMarkupContainer("plumbContainer");
+        plumbContainer.setOutputMarkupId(true);
+        plumbContainer.setOutputMarkupPlaceholderTag(true);
+        add(plumbContainer);
+
         fileUploadCanvasItemPanel = new FileUploadCanvasItemPanel("fileUploadItem");
-        add(fileUploadCanvasItemPanel);
+        plumbContainer.add(fileUploadCanvasItemPanel);
         fileUploadCanvasItemPanel.setOutputMarkupPlaceholderTag(true);
+        fileUploadCanvasItemPanel.setVisible(false);
 
         groovyScriptCanvasItemPanel = new GroovyScriptCanvasItemPanel("groovyScriptItem");
-        add(groovyScriptCanvasItemPanel);
+        plumbContainer.add(groovyScriptCanvasItemPanel);
         groovyScriptCanvasItemPanel.setOutputMarkupPlaceholderTag(true);
+        groovyScriptCanvasItemPanel.setVisible(false);
 
         tableViewCanvasItemPanel = new TableViewCanvasItemPanel("tableViewItem");
-        add(tableViewCanvasItemPanel);
+        plumbContainer.add(tableViewCanvasItemPanel);
         tableViewCanvasItemPanel.setOutputMarkupPlaceholderTag(true);
+        tableViewCanvasItemPanel.setVisible(false);
+    }
+
+    private void addLinks() {
+        fileUploadButton = new AjaxLink("fileUploadButton") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                fileUploadCanvasItemPanel.setVisible(true);
+                target.add(fileUploadCanvasItemPanel);
+                target.appendJavaScript("makeCanvasItemsDraggable('" + fileUploadCanvasItemPanel.getMarkupId() + "')");
+                target.appendJavaScript("addSourceEndpoint('" + fileUploadCanvasItemPanel.getMarkupId() + "')");
+                target.appendJavaScript("addTargetEndpoint('" + fileUploadCanvasItemPanel.getMarkupId() + "')");
+            }
+        };
+        add(fileUploadButton);
+
     }
 }
