d22cac98e6cf703bdfe7e9b4833b25896d153038
gabrielmoreno@simetrias.com <semafori2011>
2015-11-13 18:41:42 -0300

    Removed notebook id from execution API

--- portal-app/src/portal/notebook/api/DefaultCellExecutionClientConfig.java
+++ portal-app/src/portal/notebook/api/DefaultCellExecutionClientConfig.java
@@ -3,7 +3,7 @@ package portal.notebook.api;
 import javax.enterprise.inject.Alternative;
 
 @Alternative
-public class Default1CellExecutionClientConfig implements CellExecutionClientConfig {
+public class DefaultCellExecutionClientConfig implements CellExecutionClientConfig {
     @Override
     public String getBaseUri() {
         return "http://localhost:8080/ws/cell";
