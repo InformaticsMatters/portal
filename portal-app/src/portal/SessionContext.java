63bea9a2590f36b2b2edad67af83bcff6006b332
gustavosantucho <gustavosantucho@gmail.com>
2016-03-08 10:47:32 -0300

    Hook to run code the first time a user session is activated

--- portal-app/src/portal/SessionContext.java
+++ portal-app/src/portal/SessionContext.java
@@ -1,7 +1,6 @@
 package portal;
 
 import org.apache.wicket.request.cycle.RequestCycle;
-import org.squonk.security.DefaultUserDetailsManager;
 import org.squonk.security.UserDetails;
 import org.squonk.security.impl.KeycloakUserDetailsManager;
 
@@ -16,9 +15,18 @@ import java.io.Serializable;
 public class SessionContext implements Serializable {
 
     private final KeycloakUserDetailsManager defaultUserDetailsManager = new KeycloakUserDetailsManager();
+    private boolean firstTime = true;
 
     public UserDetails getLoggedInUserDetails() {
-        return defaultUserDetailsManager.getAuthenticatedUser((HttpServletRequest) RequestCycle.get().getRequest().getContainerRequest());
+        UserDetails result = defaultUserDetailsManager.getAuthenticatedUser((HttpServletRequest) RequestCycle.get().getRequest().getContainerRequest());
+        if (firstTime) {
+            runOnce(result);
+            firstTime = false;
+        }
+        return result;
     }
 
+    private void runOnce(UserDetails userDetails) {
+        // perform tasks needed when this user session first activates
+    }
 }
