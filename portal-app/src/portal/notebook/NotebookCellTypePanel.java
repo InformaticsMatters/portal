7da98fafffa5bcf05acbcfbba2185b4d7447462b
mariapaz <jonimigue2215>
2016-03-11 10:58:24 -0300

    Cells tooltips.

--- portal-app/src/portal/notebook/NotebookCellTypePanel.java
+++ portal-app/src/portal/notebook/NotebookCellTypePanel.java
@@ -1,5 +1,6 @@
 package portal.notebook;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -27,7 +28,10 @@ public class NotebookCellTypePanel extends Panel {
 
     private void addComponents() {
 
-        add(new Label("description", cellType.getDescription()));
+        Label cellName = new Label("description", cellType.getDescription());
+        add(cellName);
+
+        cellName.add(new AttributeModifier("title", cellType.getDescription()));
 
         add(new Image("cellIcon", new PackageResourceReference(PortalWebApplication.class, "resources/img2.png")));
     }
