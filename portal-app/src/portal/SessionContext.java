0bcb05467b8664f67ff30529e2f69e7856b3f051
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2016-03-14 19:08:03 +0000

    mock userclient

--- portal-app/src/portal/SessionContext.java
+++ portal-app/src/portal/SessionContext.java
@@ -1,15 +1,17 @@
 package portal;
 
-import com.im.lac.user.client.UserClient;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.squonk.client.UserClient;
+import org.squonk.core.CommonConstants;
 import org.squonk.core.user.User;
 import org.squonk.security.UserDetails;
 import org.squonk.security.impl.KeycloakUserDetailsManager;
 import org.squonk.util.IOUtils;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
@@ -24,7 +26,8 @@ public class SessionContext implements Serializable {
 
     private static final Logger logger = LoggerFactory.getLogger(SessionContext.class);
 
-    private final UserClient client = new UserClient();
+    @Inject
+    private UserClient client;
     private final KeycloakUserDetailsManager defaultUserDetailsManager = new KeycloakUserDetailsManager();
     private boolean firstTime = true;
 
@@ -32,6 +35,9 @@ public class SessionContext implements Serializable {
         logger.debug("Getting UserDetails");
         UserDetails result = defaultUserDetailsManager.getAuthenticatedUser((HttpServletRequest) RequestCycle.get().getRequest().getContainerRequest(), false);
         if (firstTime && result != null) {
+
+            System.out.println("standard-user? " +  ((HttpServletRequest) RequestCycle.get().getRequest().getContainerRequest()).isUserInRole("standard-user"));
+
             runOnce(result);
             firstTime = false;
         }
@@ -46,15 +52,16 @@ public class SessionContext implements Serializable {
 
     private void runOnce(UserDetails userDetails) {
 
+
         String authenticatedUser = userDetails.getUserid();
         if (authenticatedUser != null) {
             logger.info("Initial setup for user " + authenticatedUser);
             try {
-                User user = client.getUserObject(authenticatedUser);
+                User user = client.getUser(authenticatedUser);
                 if (user != null) {
                     logger.info("User " + user.getUsername() + " has ID " + user.getId());
                 }
-            } catch (IOException ioe) {
+            } catch (Exception ioe) {
                 logger.error("Failed to retrieve user object", ioe);
             }
         }
