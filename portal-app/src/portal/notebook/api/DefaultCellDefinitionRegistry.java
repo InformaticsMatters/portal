a4f5c88f66a650bd85700fe1e9a5b8857f656690
gabrielmoreno@simetrias.com <Semafori2013>
2016-06-03 20:54:40 -0300

    Cleanup

--- portal-app/src/portal/notebook/api/DefaultCellDefinitionRegistry.java
+++ portal-app/src/portal/notebook/api/DefaultCellDefinitionRegistry.java
@@ -97,7 +97,6 @@ public class DefaultCellDefinitionRegistry implements CellDefinitionRegistry {
     }
 
     private void loadCellDefinitions() {
-        registerCellDefinition(new TestCellDefinition());
         registerCellDefinition(new ChemblActivitiesFetcherCellDefinition());
         registerCellDefinition(createTableDisplayCellDefinition());
         registerCellDefinition(createScatterPlotCellDefinition());
