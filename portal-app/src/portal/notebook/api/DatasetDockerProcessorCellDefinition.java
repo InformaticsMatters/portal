02917e3db12a4cca0455d2a21be3022b707c1010
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2016-06-15 20:41:49 +0100

    docker cell working for sdf format data

--- portal-app/src/portal/notebook/api/DatasetDockerProcessorCellDefinition.java
+++ portal-app/src/portal/notebook/api/DatasetDockerProcessorCellDefinition.java
@@ -1,11 +1,17 @@
 package portal.notebook.api;
 
+import com.im.lac.job.jobdef.JobDefinition;
+import org.squonk.execution.steps.StepDefinition;
 import org.squonk.execution.steps.StepDefinitionConstants;
+import org.squonk.notebook.api.VariableKey;
 import org.squonk.options.MultiLineTextTypeDescriptor;
 import org.squonk.options.OptionDescriptor;
 import org.squonk.util.CommonMimeTypes;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 
@@ -13,7 +19,7 @@ import java.util.logging.Logger;
  * Created by timbo on 29/01/16.
  */
 @XmlRootElement
-public class DatasetDockerProcessorCellDefinition extends CellDefinition {
+public class DatasetDockerProcessorCellDefinition extends CellDefinition implements CommonMimeTypes {
     public static final String CELL_NAME = "Dataset Docker processor";
     private final static long serialVersionUID = 1l;
     private static final Logger LOG = Logger.getLogger(DatasetDockerProcessorCellDefinition.class.getName());
@@ -49,7 +55,91 @@ public class DatasetDockerProcessorCellDefinition extends CellDefinition {
 
     @Override
     public CellExecutor getCellExecutor() {
-        return new SimpleJobCellExecutor(StepDefinitionConstants.DockerProcessDataset.CLASSNAME);
+        return new Executor();
+    }
+
+    static class Executor extends AbstractJobCellExecutor {
+
+        @Override
+        protected JobDefinition buildJobDefinition(CellInstance cellInstance, CellExecutionData cellExecutionData) {
+
+            /*
+            In the simple case the input and output formats are MIME_TYPE_DATASET_MOLECULE_JSON so we don't need to convert formats.
+            In this case there will be just the docker executor step with input named VAR_NAME_INPUT and outout named VAR_NAME_OUTPUT
+
+            If the docker steps needs the input or output to be in different formats (currently only SDF is supported) then we need
+            to add steps before and/or after to handle this and if so we need to use temp variables (starting with underscore)
+            to pass the data between the steps.
+
+            If the input and output need converting then:
+            Step 1 (input convertor): input: VAR_NAME_INPUT, output: _docker_input
+            Step 2 (docker): input: _docker_input, output: _docker_output
+            Step 3 (output convertor): input: _docker_output, output: VAR_NAME_OUTPUT
+
+            If only input needs converting:
+            Step 1 (input convertor): input: VAR_NAME_INPUT, output: _docker_input
+            Step 2 (docker): input: _docker_input, output: VAR_NAME_OUTPUT
+
+            If only output needs converting then:
+            Step 1 (docker): input: VAR_NAME_INPUT, output: _docker_output
+            Step 2 (output convertor): input: _docker_output, output: VAR_NAME_OUTPUT
+
+            If no conversion is needed then:
+            Step 2 (output convertor): input: VAR_NAME_INPUT, output: VAR_NAME_OUTPUT
+
+             */
+
+            Long cellId = cellInstance.getId();
+            Map<String,Object> options = collectAllOptions(cellInstance);
+            String inputType = (String)options.get(OPTION_MEDIA_TYPE_INPUT);
+            String outputType = (String)options.get(OPTION_MEDIA_TYPE_OUTPUT);
+            List<StepDefinition> steps = new ArrayList<>();
+
+            String inputVarName;
+            if (!MIME_TYPE_DATASET_MOLECULE_JSON.equals(inputType)) {
+                inputVarName = "_docker_input";
+                steps.add(new StepDefinition(StepDefinitionConstants.DatasetServiceExecutor.CLASSNAME)
+                        .withInputVariableMapping(StepDefinitionConstants.VARIABLE_INPUT_DATASET, createVariableKey(cellInstance, VAR_NAME_INPUT))
+                        .withOutputVariableMapping(StepDefinitionConstants.VARIABLE_OUTPUT_DATASET, inputVarName)
+                        .withOption("header.Content-Encoding", "gzip")
+                        .withOption("header.Accept-Encoding", "gzip")
+                        .withOption("header.Content-Type", CommonMimeTypes.MIME_TYPE_DATASET_MOLECULE_JSON)
+                        .withOption("header.Accept", CommonMimeTypes.MIME_TYPE_MDL_SDF)
+                        // TODO - allow user to define which impl to use
+                        // TODO - avoid hardcoding the URL - look it up from the service descriptors?
+                        .withOption(StepDefinitionConstants.OPTION_SERVICE_ENDPOINT, "http://chemservices:8080/chem-services-cdk-basic/rest/v1/converters/convert_to_sdf"));
+            } else {
+                inputVarName = VAR_NAME_INPUT;
+            }
+            String outputVarName;
+            StepDefinition outputConvertStep = null;
+            if (!MIME_TYPE_DATASET_MOLECULE_JSON.equals(outputType)) {
+                if (MIME_TYPE_MDL_SDF.equals(outputType)) {
+                    outputVarName = "_docker_output";
+                    outputConvertStep = new StepDefinition(StepDefinitionConstants.SdfUpload.CLASSNAME)
+                            .withInputVariableMapping(StepDefinitionConstants.VARIABLE_FILE_INPUT, new VariableKey(cellId, outputVarName))
+                            .withOutputVariableMapping(StepDefinitionConstants.VARIABLE_OUTPUT_DATASET, VAR_NAME_OUTPUT);
+                } else {
+                    throw new IllegalArgumentException("Converting to media type " + outputType + " not supported");
+                }
+            } else {
+                outputVarName = VAR_NAME_OUTPUT;
+            }
+
+            // this is the docker executor step
+            steps.add(new StepDefinition(StepDefinitionConstants.DockerProcessDataset.CLASSNAME)
+                    .withInputVariableMapping(StepDefinitionConstants.VARIABLE_INPUT_DATASET, new VariableKey(cellId, inputVarName))
+                    .withOutputVariableMapping(StepDefinitionConstants.VARIABLE_OUTPUT_DATASET, outputVarName)
+                    .withOptions(options));
+
+            if (outputConvertStep != null) {
+                steps.add(outputConvertStep);
+            }
+
+            LOG.info("Docker cell using variables " + inputVarName + " and " + outputVarName);
+
+            return buildJobDefinition(cellExecutionData, cellInstance, steps.toArray(new StepDefinition[steps.size()]));
+        }
     }
 
 }
