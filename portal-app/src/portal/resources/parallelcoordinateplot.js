ed0b4dd46d68f8b32a46a26f86787e35240ca074
Tim Dudgeon <tudgeon@informaticsmatters.com>
2016-07-14 09:16:27 +0100

    applying PCPlot changes to portal

--- portal-app/src/portal/resources/parallelcoordinateplot.js
+++ portal-app/src/portal/resources/parallelcoordinateplot.js
@@ -2,57 +2,53 @@
 
 function buildParallelCoordinatePlot(id, data) {
 
-        var svgSelector = "#" + id + " .svg-container";
-        d3.select(svgSelector).selectAll("*").remove();
+    var svgSelector = "#" + id + " .svg-container"
+    d3.select(svgSelector).selectAll("*").remove();
 
-        if (data.length == 0 || data[0] == null) {
-            return;
-        }
+    if (data[0] == null) {
+        //console.log("No data - clearing and returning");
+        return;
+    }
 
-        //console.log("id: " +id);
-        //console.log("Num Data items:" + data.length);
+    //console.log("Num Data items:" + data.length)
 
-        var generateColorScale = function(dimension) {
-            return d3.scale.linear()
-                .domain([d3.min(data, function(d) { return d[dimension]; }), d3.max(data, function(d) { return d[dimension]; })])
-                .range(["steelblue", "brown"])
-                .interpolate(d3.interpolateLab);
-        };
+    function generateColorScale(data, dimension) {
+        var min = d3.min(data, function(d) { return d[dimension]; });
+        var max = d3.max(data, function(d) { return d[dimension]; });
+        //console.log("Dimension: " + dimension + " Range: " + min + " - " + max);
+        return d3.scale.linear()
+            .domain([min, max])
+            .range(["steelblue", "brown"])
+            .interpolate(d3.interpolateLab);
+    };
 
-        // quantitative color scale
-        var scale = generateColorScale('idx');
+    var scale = generateColorScale(data, 'idx');
 
-        var color = function(d) { return scale(d.idx); };
+    var parcoords = d3.parcoords()(svgSelector)
+        .color(function(d) { return scale(d.idx); })
+        .alpha(0.4)
+        .data(data)
+        .hideAxis(["uuid"])
+        .composite("darker")
+        .render()
+        .shadows()
+        .reorderable()
+        .brushMode("1D-axes");  // enable brushing
 
-        var parcoords = d3.parcoords()(svgSelector)
-            .color(color)
-            .alpha(0.4)
-            .data(data)
-            .hideAxis(["uuid"])
-            .composite("darker")
-            .render()
-            .shadows()
-            .reorderable()
-            .brushMode("1D-axes");  // enable brushing
+    // click label to activate coloring
+    parcoords.svg.selectAll(".dimension")
+        .on("click", change_color);
 
-//            // click label to activate coloring
-//            parcoords.svg.selectAll(".dimension")
-//                .on("click", change_color)
-//                .selectAll(".label")
-//                .style("font-size", "12px");
-//
-//            // update color
-//            function change_color(dimension) {
-//              console.log("Clicked " + dimension);
-//              parcoords.svg.selectAll(".dimension")
-//                .style("font-weight", "normal")
-//                .filter(function(d) { return d == dimension; })
-//                .style("font-weight", "bold");
-//
-//              colorByAxis = dimension;
-//              console.log("Coloring by " + dimension);
-//
-//              parcoords.color(generateColorScale(dimension)).render();
-//            }
+    // update color
+    function change_color(dimension) {
+        parcoords.svg.selectAll(".dimension")
+            .style("font-weight", "normal")
+            .filter(function(d) { return d == dimension; })
+            .style("font-weight", "bold");
+
+        scale = generateColorScale(parcoords.data(), dimension);
+        parcoords.color(function(d) { return scale(d[dimension]); })
+            .render();
+    }
 
 }
