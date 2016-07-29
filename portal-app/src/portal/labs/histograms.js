16ec7a5190ecab43e754053d3c6f1d2d2b96d236
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2016-07-29 15:28:05 +0100

    initial work to make histograms less hard-coded

--- portal-app/src/portal/labs/histograms.js
+++ portal-app/src/portal/labs/histograms.js
@@ -5,7 +5,7 @@ var get_random_data	= function() {
 		  return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
 
-		for(var i = 0; i < 10000; i++) {
+		for(var i = 0; i < 1000; i++) {
 			data.push({
 				id: i,
 				a: getRandomInt(0,49),
@@ -18,28 +18,47 @@ var get_random_data	= function() {
 		return data;
 }
 
-function execute() {
-		var filter	= crossfilter(get_random_data());
+function execute(id, config, data) {
 
-		var data = {
-			a: { dimension: filter.dimension(function(d){ return d.a; }) },
-			b: { dimension: filter.dimension(function(d){ return d.b; }) },
-			c: { dimension: filter.dimension(function(d){ return d.c; }) },
-			d: { dimension: filter.dimension(function(d){ return d.d; }) }
-		};
+        var fields = config.fields,
+            bins = config.bins ? config.bins : 50;
 
-		data.a.group = data.a.dimension.group(function(d){ return Math.floor(d / 1)*1; });
-		data.b.group = data.b.dimension.group(function(d){ return Math.floor(d / 10)*10; });
-		data.c.group = data.c.dimension.group(function(d){ return Math.floor(d / 100)*100; });
-		data.d.group = data.d.dimension.group(function(d){ return Math.floor(d / 1000)*1000; });
+        var outer = d3.select("#"+id);
+        outer.selectAll("*").remove();
 
-		var a = ay_histogram('histogram-a', data.a, {margin: [10, 10], bin_width: 1, tick_width: 75});
-		var b = ay_histogram('histogram-b', data.b, {margin: [10, 10], bin_width: 10, tick_width: 75});
-		var c = ay_histogram('histogram-c', data.c, {margin: [10, 10], bin_width: 100, tick_width: 75});
-		var d = ay_histogram('histogram-d', data.d, {margin: [10, 10], bin_width: 1000, tick_width: 75});
+        var filter	= crossfilter(data);
 
-		a.setRelations([b,c,d]);
-		b.setRelations([a,c,d]);
-		c.setRelations([a,b,d]);
-		d.setRelations([a,b,c]);
+        var xdata = {};
+        var histograms = [];
+        for (i=0; i<config.fields.length; i++) {
+            var name = fields[i];
+            var div = outer.append("div").attr("class", "column");
+            div.append("div").attr("class", "column-title").text(name);
+            div.append("svg").attr("class", "histogram-" + name);
+
+            xdata[name] = { dimension: filter.dimension(function(d){ return d[name]; }) }
+
+            var min = d3.min(data, function(d) { return d[name] });
+            var max = d3.max(data, function(d) { return d[name] });
+            var binWidth = Math.round((max - min) / bins);
+            console.log("MinMax[" + name + "]: " + min + " " + max + " -> " + binWidth);
+            xdata[name].group = xdata[name].dimension.group(function(x){
+                var bin = Math.floor(x / binWidth) * binWidth;
+                console.log(x + " -> " + bin);
+                return bin;
+            });
+
+            histograms.push(ay_histogram('histogram-' + name, xdata[name], {margin: [10, 10], bin_width: binWidth, tick_width: 75}));
+        }
+
+        for (i=0; i<histograms.length; i++) {
+            var arr = []
+            for (j=0; j<histograms.length; j++) {
+                if (i != j) {
+                    arr.push(histograms[j]);
+                }
+            }
+            histograms[i].setRelations(arr);
+        }
+
 }
