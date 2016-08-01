e84c5e6e067754bcc752112c7d709bb820bd69c6
Tim Dudgeon <tudgeon@informaticsmatters.com>
2016-08-01 14:08:31 +0100

    heatmap now working ok

--- portal-app/src/portal/labs/heatmap.js
+++ portal-app/src/portal/labs/heatmap.js
@@ -1,19 +1,55 @@
 
-var execute = function(config, data) {
+var execute = function(selection, config, data) {
 
     var colorScale = config.colorScale ? config.colorScale :
             d3.scale.linear()
-                .domain([d3.min(data, function(d) { return d.value}), d3.max(data, function(d) { return d.value})])
+                .domain(d3.extent(data, function(d) { return d.value}))
                 .range(['white', 'brown']),
         margin = config.margin ? config.margin : { top: 75, right: 50, bottom: 50, left: 75 },
-        cellSize = config.cellSize ? config.cellSize : 12,
-        rowNames = config.rowNames,
-        colNames = config.colNames,
-        width = cellSize * colNames.length,
-        height = cellSize * rowNames.length,
-        transition = config.transition != null ? config.transition : 1000;
+        cellSize = config.cellSize ? config.cellSize : 12, // size of cells
+        rows = config.rowNames.map(function(n) { return {name:n, sum:0, count:0}; }), // details of the rows
+        cols = config.colNames.map(function(n) { return {name:n, sum:0, count:0}; }), // ... and columns
+        width = cellSize * cols.length,
+        height = cellSize * rows.length,
+        transition = config.transition != null ? config.transition : 1000 // speed of transition when sorting
+        rowIndexes = d3.range(rows.length), // row indexes allow the correct row to be loacted after sorting
+        colIndexes = d3.range(cols.length); // ... and for columns
 
-  var svg = d3.select("#chart").append("svg")
+
+    // gather sums and counts
+    data.forEach(function(d) {
+        rows[d.row].count++;
+        rows[d.row].sum += d.value;
+        cols[d.col].count++;
+        cols[d.col].sum += d.value;
+    });
+
+    // sort function
+    function sorter(a, b) {
+            if (a.val !== null && b.val !== null) return a.val - b.val;
+            if (a.val === null && b.val === null) return 0;
+            if (a.val === null) return -1;
+            if (b.val === null) return 1;
+    }
+
+    // function to generate sort by average position
+    function sortByAverages(elements) {
+        elements.forEach(function(element, i) {
+            var c = element.count;
+            if (c > 0) {
+                element.avg = element.sum / c;
+            }
+        });
+        var sortedElements = elements.map(function(d,i) { return {idx:i, val:d.avg}}).sort(sorter);
+        sortedElements.forEach(function(e,i) { elements[e.idx].sortedByAvg = i});
+    }
+
+    // generate the sort by average indices
+    sortByAverages(rows);
+    sortByAverages(cols);
+
+  d3.select(selection).selectAll("*").remove();
+  var svg = d3.select(selection).append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
@@ -23,7 +59,7 @@ var execute = function(config, data) {
   var colSortOrder=false;
   var rowLabels = svg.append("g")
       .selectAll(".rowLabelg")
-      .data(rowNames)
+      .data(rows.map(function(d) { return d.name }))
       .enter()
       .append("text")
       .text(function (d) { return d; })
@@ -34,11 +70,11 @@ var execute = function(config, data) {
       .attr("class", function (d,i) { return "rowLabel mono r"+i;} )
       .on("mouseover", function(d) {d3.select(this).classed("text-hover",true);})
       .on("mouseout" , function(d) {d3.select(this).classed("text-hover",false);})
-      .on("click", function(d,i) {rowSortOrder=!rowSortOrder; sortbylabel("r",i,rowSortOrder);d3.select("#order").property("selectedIndex", 4).node().focus();;});
+      .on("click", function(d,i) {rowSortOrder=!rowSortOrder; sortbylabel("r",i,rowSortOrder);});
 
   var colLabels = svg.append("g")
       .selectAll(".colLabelg")
-      .data(colNames)
+      .data(cols.map(function(d) { return d.name }))
       .enter()
       .append("text")
       .text(function (d) { return d; })
@@ -49,7 +85,7 @@ var execute = function(config, data) {
       .attr("class",  function (d,i) { return "colLabel mono c"+i;} )
       .on("mouseover", function(d) {d3.select(this).classed("text-hover",true);})
       .on("mouseout" , function(d) {d3.select(this).classed("text-hover",false);})
-      .on("click", function(d,i) {colSortOrder=!colSortOrder;  sortbylabel("c",i,colSortOrder);d3.select("#order").property("selectedIndex", 4).node().focus();;});
+      .on("click", function(d,i) {colSortOrder=!colSortOrder;  sortbylabel("c",i,colSortOrder);});
 
   var heatMap = svg.append("g").attr("class","g3")
         .selectAll(".cellg")
@@ -62,14 +98,6 @@ var execute = function(config, data) {
         .attr("width", cellSize)
         .attr("height", cellSize)
         .style("fill", function(d) { return colorScale(d.value); })
-        /* .on("click", function(d) {
-               var rowtext=d3.select(".r"+(d.row-1));
-               if(rowtext.classed("text-selected")==false){
-                   rowtext.classed("text-selected",true);
-               }else{
-                   rowtext.classed("text-selected",false);
-               }
-        })*/
         .on("mouseover", function(d){
                //highlight text
                d3.select(this).classed("cell-hover",true);
@@ -81,7 +109,7 @@ var execute = function(config, data) {
                  .style("left", (d3.event.pageX+10) + "px")
                  .style("top", (d3.event.pageY-10) + "px")
                  .select("#value")
-                 .html("labels: "+rowNames[d.row]+", "+colNames[d.col]+"<br>value: "+d.value+"<br>row,col: "+d.col+", "+d.row+"<br>cell x,y: "+this.x.baseVal.value+", "+this.y.baseVal.value);
+                 .html("labels: " + rows[d.row].name + ", " + cols[d.col].name + "<br>value: "+d.value+"<br>row,col: "+d.col+", "+d.row+"<br>cell x,y: "+this.x.baseVal.value+", "+this.y.baseVal.value);
                //Show the tooltip
                d3.select("#tooltip").classed("hidden", false);
         })
@@ -92,94 +120,110 @@ var execute = function(config, data) {
                d3.select("#tooltip").classed("hidden", true);
         });
 
-//  var legend = svg.selectAll(".legend")
-//      .data([-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10])
-//      .enter().append("g")
-//      .attr("class", "legend");
-//
-//  legend.append("rect")
-//    .attr("x", function(d, i) { return legendElementWidth * i; })
-//    .attr("y", height+(cellSize*2))
-//    .attr("width", legendElementWidth)
-//    .attr("height", cellSize)
-//    .style("fill", function(d, i) { return colors[i]; });
-//
-//  legend.append("text")
-//    .attr("class", "mono")
-//    .text(function(d) { return d; })
-//    .attr("width", legendElementWidth)
-//    .attr("x", function(d, i) { return legendElementWidth * i; })
-//    .attr("y", height + (cellSize*4));
-
 // Change ordering of cells
 
-  function sortbylabel(rORc,i,sortOrder){
-       var t = svg.transition().duration(transition);
-       var values=[];
-       var sorted; // sorted is zero-based index
-       d3.selectAll(".c"+rORc+i)
-         .filter(function(ce){
-            values.push(ce.value);
-          });
-       if (rORc=="r") { // sort by row
-         sorted=d3.range(colNames.length).sort(function(a,b){ if (sortOrder) { return values[b]-values[a];} else { return values[a]-values[b];}});
-         t.selectAll(".cell")
-           .attr("x", function(d) { return sorted.indexOf(d.col) * cellSize; });
-         t.selectAll(".colLabel")
-          .attr("y", function (d, i) { return sorted.indexOf(i) * cellSize; });
+    function sortbylabel(rORc,i,sortOrder){
+        var t = svg.transition().duration(transition);
+        var values=[];
+        var sorted; // sorted is zero-based index
+        d3.selectAll(".c"+rORc+i)
+            .filter(function(ce) { values.push(ce.value); });
+        if (rORc=="r") { // sort by row
+            sorted=d3.range(cols.length).sort(function(a,b){ if (sortOrder) { return values[b]-values[a];} else { return values[a]-values[b];}});
+            t.selectAll(".cell")
+                .attr("x", function(d) { return sorted.indexOf(d.col) * cellSize; });
+            t.selectAll(".colLabel")
+                .attr("y", function (d, i) { return sorted.indexOf(i) * cellSize; });
+            rowIndexes = sorted;
+            //console.log("Rows: " + JSON.stringify(sorted));
        } else { // sort by column
-         sorted=d3.range(rowNames.length).sort(function(a,b){if (sortOrder) { return values[b]-values[a];} else { return values[a]-values[b];}});
-         t.selectAll(".cell")
-           .attr("y", function(d) { return sorted.indexOf(d.row) * cellSize; });
-         t.selectAll(".rowLabel")
-          .attr("y", function (d, i) { return sorted.indexOf(i) * cellSize; });
+            sorted=d3.range(rows.length).sort(function(a,b){if (sortOrder) { return values[b]-values[a];} else { return values[a]-values[b];}});
+            t.selectAll(".cell")
+                .attr("y", function(d) { return sorted.indexOf(d.row) * cellSize; });
+            t.selectAll(".rowLabel")
+                .attr("y", function (d, i) { return sorted.indexOf(i) * cellSize; });
+            colIndexes = sorted;
+            //console.log("Cols: " + JSON.stringify(sorted));
        }
   }
 
-  d3.select("#order").on("change",function(){
-    order(this.value);
-  });
+    d3.select("#resetrows").on("click",function(){ order("byrowdefault"); });
+    d3.select("#resetcols").on("click",function(){ order("bycoldefault"); });
+    d3.select("#sortbyrowavg").on("click",function(){ order("byrowavg"); });
+    d3.select("#sortbycolavg").on("click",function(){ order("bycolavg"); });
 
-  function order(value){
-   if(value=="default"){
-    var t = svg.transition().duration(transition);
-    t.selectAll(".cell")
-      .attr("x", function(d) { return d.col * cellSize; })
-      .attr("y", function(d) { return d.row * cellSize; }) ;
+    var byrowavgDesc = false;
+    var bycolavgDesc = false;
+    function order(value){
+        if (value=="byrowdefault"){
+            var t = svg.transition().duration(transition);
+            t.selectAll(".cell")
+                .attr("y", function(d) { return (d.row) * cellSize; });
 
-    t.selectAll(".rowLabel")
-      .attr("y", function (d, i) { return i * cellSize; });
+            t.selectAll(".rowLabel")
+                .attr("y", function (d, i) { return i * cellSize; });
 
-    t.selectAll(".colLabel")
-      .attr("y", function (d, i) { return i * cellSize; });
+            rowIndexes = d3.range(rows.length);
+            //console.log("Rows: " + JSON.stringify(rowIndexes));
 
-   } else if (value=="byrowandcol"){
-    var t = svg.transition().duration(transition);
-    t.selectAll(".cell")
-      .attr("x", function(d) { return (d.col) * cellSize; })
-      .attr("y", function(d) { return (d.row) * cellSize; });
+        } else if (value=="bycoldefault"){
+            var t = svg.transition().duration(transition);
+            t.selectAll(".cell")
+                .attr("x", function(d) { return (d.col) * cellSize; });
+            t.selectAll(".colLabel")
+                .attr("y", function (d, i) { return i * cellSize; }) ;
 
-    t.selectAll(".rowLabel")
-      .attr("y", function (d, i) { return i * cellSize; });
+            colIndexes = d3.range(cols.length);
+            //console.log("Cols: " + JSON.stringify(colIndexes));
 
-    t.selectAll(".colLabel")
-      .attr("y", function (d, i) { return i * cellSize; });
+        } else if (value=="byrowavg"){
+            var t = svg.transition().duration(transition);
 
-   } else if (value=="byrow"){
-    var t = svg.transition().duration(transition);
-    t.selectAll(".cell")
-      .attr("y", function(d) { return (d.row) * cellSize; });
+            t.selectAll(".cell")
+                .attr("y", function(d) {
+                    var idx = rows[d.row].sortedByAvg;
+                    return (byrowavgDesc ? idx : rows.length - idx) * cellSize;
+                });
 
-    t.selectAll(".rowLabel")
-      .attr("y", function (d, i) { return i * cellSize; });
-   } else if (value=="bycol"){
-    var t = svg.transition().duration(transition);
-    t.selectAll(".cell")
-      .attr("x", function(d) { return (d.col) * cellSize; });
-    t.selectAll(".colLabel")
-      .attr("y", function (d, i) { return i * cellSize; }) ;
-   }
-  }
+            t.selectAll(".rowLabel")
+                .attr("y", function (d, i) {
+                    var idx = rows[i].sortedByAvg;
+                    return (byrowavgDesc ? idx : rows.length - idx) * cellSize;
+                });
+
+            var indexes = Array(rows.length);
+            rows.forEach(function(d,i) {
+                indexes[byrowavgDesc ? d.sortedByAvg : rows.length - d.sortedByAvg - 1] =  i;
+            });
+            rowIndexes = indexes;
+            //console.log("Rows: " + JSON.stringify(indexes));
+
+            byrowavgDesc = !byrowavgDesc;
+
+        } else if (value=="bycolavg"){
+            var t = svg.transition().duration(transition);
+            t.selectAll(".cell")
+                .attr("x", function(d) {
+                    var idx = cols[d.col].sortedByAvg;
+                    return (bycolavgDesc ? idx : cols.length - idx) * cellSize;
+                });
+
+            t.selectAll(".colLabel")
+                .attr("y", function (d, i) {
+                    var idx = cols[i].sortedByAvg;
+                    return (bycolavgDesc ? idx : cols.length - idx) * cellSize;
+                });
+
+            var indexes = Array(cols.length);
+            cols.forEach(function(d,i) {
+                indexes[bycolavgDesc ? d.sortedByAvg : cols.length - d.sortedByAvg - 1] =  i;
+            });
+            colIndexes = indexes;
+            //console.log("Cols: " + JSON.stringify(indexes));
+
+            bycolavgDesc = !bycolavgDesc;
+        }
+    }
 
   var sa=d3.select(".g3")
       .on("mousedown", function() {
@@ -214,17 +258,16 @@ var execute = function(config, data) {
                  move = {
                      x : p[0] - d.x,
                      y : p[1] - d.y
-                 }
-             ;
+                 };
 
-             if(move.x < 1 || (move.x*2<d.width)) {
+             if (move.x < 1 || (move.x*2<d.width)) {
                  d.x = p[0];
                  d.width -= move.x;
              } else {
                  d.width = move.x;
              }
 
-             if(move.y < 1 || (move.y*2<d.height)) {
+             if (move.y < 1 || (move.y*2<d.height)) {
                  d.y = p[1];
                  d.height -= move.y;
              } else {
@@ -232,7 +275,7 @@ var execute = function(config, data) {
              }
              s.attr(d);
 
-                 // deselect all temporary selected state objects
+             // deselect all temporary selected state objects
              d3.selectAll('.cell-selection.cell-selected').classed("cell-selected", false);
              d3.selectAll(".text-selection.text-selected").classed("text-selected",false);
 
@@ -248,11 +291,11 @@ var execute = function(config, data) {
                      .classed("cell-selection", true)
                      .classed("cell-selected", true);
 
-                     d3.select(".r"+(cell_d.row-1))
+                     d3.select(".r"+(cell_d.row))
                      .classed("text-selection",true)
                      .classed("text-selected",true);
 
-                     d3.select(".c"+(cell_d.col-1))
+                     d3.select(".c"+(cell_d.col))
                      .classed("text-selection",true)
                      .classed("text-selected",true);
                  }
@@ -260,10 +303,10 @@ var execute = function(config, data) {
          }
       })
       .on("mouseup", function() {
-            // remove selection frame
+         // remove selection frame
          sa.selectAll("rect.selection").remove();
 
-             // remove temporary selection marker class
+         // remove temporary selection marker class
          d3.selectAll('.cell-selection').classed("cell-selection", false);
          d3.selectAll(".text-selection").classed("text-selection",false);
       })
