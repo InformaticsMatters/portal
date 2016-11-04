3d9408a7f2cf913ebc553dd4fd484d2c147bdc4a
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2016-11-04 10:16:15 +0000

    fixed issue with bad results viewers

--- portal-app/src/portal/notebook/webapp/CanvasItemPanel.java
+++ portal-app/src/portal/notebook/webapp/CanvasItemPanel.java
@@ -73,7 +73,8 @@ public abstract class CanvasItemPanel extends Panel implements CellTitleBarPanel
             for (VariableDefinition varDef : varDefs) {
                 String name = varDef.getName();
                 VariableType type = varDef.getVariableType();
-                if (VariableType.DATASET_ANY.supports(VariableType.DATASET_MOLS)) {
+                if (VariableType.DATASET_ANY.supports(type)) {
+                    LOGGER.fine("Creating results handler for variable " + name + " in cell " + cellInstance.getName());
                     resultsHandler = new DatasetResultsHandler(name, notebookSession, structureIOClient, cellInstance.getId());
                     return;
                 }
